83b9b8befaf6b0a44099179f39f415eaf03afbd7
Halogen96 <rachitnimje9124@gmail.com>
2025-04-04 02:48:17 +0530

    updated user model

--- models/user.go
+++ models/user.go
@@ -2,15 +2,15 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"time"
 )
 
 type User struct {
-	Id        uint      `gorm:"primaryKey;autoIncrement"`
-	Username  string    `gorm:"unique"`
-	Name      string    `gorm:"not null"`
-	Password  string    `gorm:"not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
+	//Id        uint      `gorm:"primaryKey;autoIncrement"`
+	gorm.Model
+	Username string `gorm:"unique"`
+	Name     string `gorm:"not null"`
+	Password string `gorm:"not null"`
+	//CreatedAt time.Time `gorm:"autoCreateTime"`
 }
 
 func Migrate(db *gorm.DB) error {
